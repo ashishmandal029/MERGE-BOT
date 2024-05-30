3747586a8eea84b143588a5bb2e6ab4d79530051
ashishmandal029 <135951869+ashishmandal029@users.noreply.github.com>
2024-05-30 07:04:11 +0530

    config.py

--- config.py
+++ config.py
@@ -2,15 +2,15 @@ import os
 
 
 class Config(object):
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    TELEGRAM_API = os.environ.get("TELEGRAM_API")
-    OWNER = os.environ.get("OWNER")
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
-    PASSWORD = os.environ.get("PASSWORD")
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
-    GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
+    API_HASH = os.environ.get("e4195bedc71234a179a3d9ac0cad6401")
+    BOT_TOKEN = os.environ.get("7254134104:AAEp5yxJMnTSQGyJ3kXxfVh65hlVyAjITf8")
+    TELEGRAM_API = os.environ.get("24871620")
+    OWNER = os.environ.get("5728398903")
+    OWNER_USERNAME = os.environ.get("ashish")
+    PASSWORD = os.environ.get("8472")
+    DATABASE_URL = os.environ.get("mongodb+srv://ashisham029:<Teacher@274>@cluster0.2pxqhuu.mongodb.net/?retryWrites=true&w=majority")
+    LOGCHANNEL = os.environ.get("-1002017765276")  # Add channel id as -100 + Actual ID
+    GDRIVE_FOLDER_ID = os.environ.get("1-kv8FIMhRLMWe_jF3-v__qXzLF3o9f4q", "root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = False
     MODES = ["video-video", "video-audio", "video-subtitle", "extract-streams"]
